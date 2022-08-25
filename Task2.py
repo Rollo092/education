e7bb14c9417787002bac1d1eba4a7936916e6d60
Rollo092 <90210350+Rollo092@users.noreply.github.com>
2022-08-25 10:47:49 +0400

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,34 @@
+import sys
+import re
+Link = (sys.argv[1])
+Files = open(Link)
+Massiv = []
+for i in Files:
+    if re.search(r'\d' ,i) != None:
+        Massiv.append(int(i))
+    else:
+        print('Ошибка в строке. Значение != Цифре')
+        quit()
+count = 0
+SredZnach = 0
+for i in Massiv:
+    SredZnach = i+SredZnach
+SredZnach = SredZnach/len(Massiv)
+SredZnach = SredZnach.__floor__()
+
+while True:
+    for i in Massiv:
+        ind = Massiv.index(i)
+        if i < SredZnach:
+            Massiv[ind] = i+1
+            count = count+1
+        elif i > SredZnach:
+            Massiv[ind] = i - 1
+            count = count + 1
+        elif i == SredZnach:
+            continue
+        if len(set(Massiv)) == 1:
+            break
+    if len(set(Massiv)) == 1:
+        break
+print(count)
